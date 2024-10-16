62f9f74834f1e9cf886f0338732e6e064525c5c7
ThePhysic <57155067+ThePhysic@users.noreply.github.com>
2024-10-15 19:05:05 -0700

    Update script.js

--- Project 1/script.js
+++ Project 1/script.js
@@ -1,6 +1,6 @@
 document.getElementById("contact-form").addEventListener("submit", function (event) {
-    var name = document.getElementById("name").value;
-    var email = document.getElementById("email").value;
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
 
     if (name === "" || email === "") {
       alert("Please fill in all required fields.");
